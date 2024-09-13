969f4bbad98e8898a086560c0843de54b2d11743
tarun3.tataaig <tarun3.tataaig@gmail.com>
2024-09-13 10:52:36 +0000

    Day 5: Question No 171 (Easy String 171 Excel Sheet Column Number)

---
+++ DSA String Easy/excelSheetColumnNumber.js
@@ -0,0 +1,14 @@
+//171. Excel Sheet Column Number
+
+var titleToNumber = function(columnTitle) {
+    let result = 0;
+    
+    for (let i = 0; i < columnTitle.length; i++) {
+        const charValue = columnTitle.charCodeAt(i) - 'A'.charCodeAt(0) + 1;
+        result = result * 26 + charValue;
+    }
+
+    return result;
+};
+
+console.log(titleToNumber('AAA'))

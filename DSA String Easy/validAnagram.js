2b6113df011b6e14444ce20db3fa77ec66edc855
tarun3.tataaig <tarun3.tataaig@gmail.com>
2024-09-16 16:48:39 +0000

    Day 7: Question No 242 (Easy String 242. Valid Anagram)

---
+++ DSA String Easy/validAnagram.js
@@ -0,0 +1,24 @@
+// 242. Valid Anagram
+
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) {
+        return false;
+    };
+    
+    const charCount = {};
+
+    for (let char of s) {
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+  
+    for (let char of t) {
+      if (!charCount[char]) {
+        return false;
+      }
+      charCount[char]--;
+    }
+  
+    return true;
+};
+
+console.log(isAnagram("anagram", "nagaram"))

83135048e0992264888de0b93f33284cbb49ca51
Tarun K. Gupta <tg.github@gmail.com>
2026-03-04 13:54:36 +0530

    Valid Palindrome - done

--- fundamentals/two-pointers.js
+++ fundamentals/two-pointers.js
@@ -211,3 +211,28 @@ var isAnagram = function (s, t) {
 };
 
 console.log(isAnagram("anagram", "nagaram"));
+
+// Question 10 - 125. Valid Palindrome
+var isPalindrome = function (s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    while (left < right && !isAlphaNumeric(s[left])) left++;
+
+    while (left < right && !isAlphaNumeric(s[right])) right--;
+
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
+
+function isAlphaNumeric(char) {
+  return /^[a-z0-9]$/i.test(char);
+}
+
+isPalindrome("A man, a plan, a canal: Panama");

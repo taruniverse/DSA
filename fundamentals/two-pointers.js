3171885a9ef15bae77f5a3710afc4c6133413187
Tarun K. Gupta <tg.github@gmail.com>
2026-03-05 19:03:25 +0530

    121. Best Time to Buy and Sell Stock - done

--- fundamentals/two-pointers.js
+++ fundamentals/two-pointers.js
@@ -236,3 +236,19 @@ function isAlphaNumeric(char) {
 }
 
 isPalindrome("A man, a plan, a canal: Panama");
+
+// Question 11 - 121. Best Time to Buy and Sell Stock
+var maxProfit = function (prices) {
+  let buy = prices[0];
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < buy) {
+      buy = prices[i];
+    } else if (prices[i] - buy > profit) {
+      profit = prices[i] - buy;
+    }
+  }
+  return profit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
